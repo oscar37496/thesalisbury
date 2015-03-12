5d568444ab44e74f1fb4e2a2b48962e3d2fb9478
oscarmcnulty <oscar37496@hotmail.com>
2015-03-12 21:55:06 +1100

    Added currency sort plugin to datatables

--- app/views/admin/users.blade.php
+++ app/views/admin/users.blade.php
@@ -99,6 +99,20 @@ $("#"+id).load(url)
 			"bAutoWidth" : false
 		});
 	}); 
+	jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+	    "currency-pre": function ( a ) {
+	        a = (a==="-") ? 0 : a.replace( /[^\d\-\.]/g, "" );
+	        return parseFloat( a );
+	    },
+	 
+	    "currency-asc": function ( a, b ) {
+	        return a - b;
+	    },
+	 
+	    "currency-desc": function ( a, b ) {
+	        return b - a;
+	    }
+	} );
 </script>
 @stop
 
