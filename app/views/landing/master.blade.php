2eb5ec0e8bdb1d3a1f1a92a97c08e266b4d61a9f
oscarmcnulty <oscar37496@hotmail.com>
2015-05-22 18:53:04 +1000

    Removed team section due to honi gimps

--- app/views/landing/master.blade.php
+++ app/views/landing/master.blade.php
@@ -260,78 +260,7 @@
         </div>
     </section>
 
-    <!-- Team Section -->
-    <section id="team" class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">The 2015 Syndicate</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/743762199/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Alex</h4>
-                        <p class="text-muted">Juris Doctor</p>
-                       <!-- <ul class="list-inline social-buttons">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>-->
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/825565178/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Daniel</h4>
-                        <p class="text-muted">Bachelor of Civil Engineering/Bachelor of Commerce</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/560298673/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>George</h4>
-                        <p class="text-muted">Bachelor of Economics</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/784987855/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Jack</h4>
-                        <p class="text-muted">Bachelor of Arts</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/543809016/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Lachlan</h4>
-                        <p class="text-muted">Bachelor of Commerce</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/1186296357/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Oscar</h4>
-                        <p class="text-muted">Bachelor of Aerospace Engineering/Bachelor of Science</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="team-member">
-                        <img src="https://graph.facebook.com/v2.2/653796752/picture?height=180&amp;width=180" class="img-responsive img-circle" alt="">
-                        <h4>Rob</h4>
-                        <p class="text-muted">Juris Doctor</p>
-                    </div>
-                </div>
-            </div>
-            <!-- <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-                </div>
-            </div> -->
-        </div>
-    </section>
+    
 
     <!-- Clients Aside -->
     <!--<aside class="clients">
