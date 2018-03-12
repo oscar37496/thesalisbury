0fb5a43c6f2bc6cb235a1563f4c41b95c046667c
Oscar McNulty <oscar37496@hotmail.com>
2018-03-12 13:20:23 +1100

    Pass user id as string in ajax return.

--- app/views/admin/ajax/users.blade.php
+++ app/views/admin/ajax/users.blade.php
@@ -4,20 +4,20 @@
 <td>{{{ $user['last_name'] }}}</td>
 <td> @if($user['is_activated'])
 	Activated
-	<button type="button" onclick="loadAjax({{{ $user['id'] }}}, '/account/admin/users/{{{ $user['id'] }}}/deactivate')">
+	<button type="button" onclick="loadAjax('{{{ $user['id'] }}}', '/account/admin/users/{{{ $user['id'] }}}/deactivate')">
 		Deactivate
 	</button> @else
 	Deactivated
-	<button type="button" onclick="loadAjax({{{ $user['id'] }}}, '/account/admin/users/{{{ $user['id'] }}}/activate')">
+	<button type="button" onclick="loadAjax('{{{ $user['id'] }}}', '/account/admin/users/{{{ $user['id'] }}}/activate')">
 		Activate
 	</button> @endif </td>
 <td> @if($user['is_social'])
 	Social
-	<button type="button" onclick="loadAjax({{{ $user['id'] }}}, '/account/admin/users/{{{ $user['id'] }}}/remove-social')">
+	<button type="button" onclick="loadAjax('{{{ $user['id'] }}}', '/account/admin/users/{{{ $user['id'] }}}/remove-social')">
 		Remove Social
 	</button> @else
 	Not Social
-	<button type="button" onclick="loadAjax({{{ $user['id'] }}}, '/account/admin/users/{{{ $user['id'] }}}/make-social')">
+	<button type="button" onclick="loadAjax('{{{ $user['id'] }}}', '/account/admin/users/{{{ $user['id'] }}}/make-social')">
 		Make Social
 	</button> @endif </td>
 <td>{{{ money_format('%n', $total_spent_last_week / 100 ) }}}</td>
